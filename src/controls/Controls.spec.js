d2e52684f9aaeb0dffa526bb599e2dce66f56140
efleming111 <efleming111@yahoo.com>
2019-02-05 21:02:35 -0700

    Finished MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,23 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from './Controls';
+
+describe('the Controls component', ()=>{
+    it('renders the component', ()=>{
+        render(<Controls/>);
+    })
+
+    it('displays Close gate button', ()=>{
+        const {getByText} = render(<Controls locked={false} closed={false}/>);
+
+        getByText(/close gate/i);
+    })
+
+    it('pressed gate button', ()=>{
+        const {getByText} = render(<Controls locked={false} closed={false}/>);
+        
+        fireEvent.click(getByText(/close gate/i));
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -5,12 +5,19 @@ import 'jest-dom/extend-expect';
 import Display from './Display';
 
 describe('the Display component', ()=>{
-    it('it renders the component', ()=>{
+    it('renders the component', ()=>{
         render(<Display/>);
     })
 
     it('defaults to unlocked and open', ()=>{
         const {getByText} = render(<Display/>);
+
+        getByText(/unlocked/i);
+        getByText(/open/i);
+    })
+
+    it('displays Locked and Closed', ()=>{
+        const {getByText} = render(<Display closed={true} locked={true}/>);
 
         getByText(/unlocked/i);
         getByText(/open/i);
